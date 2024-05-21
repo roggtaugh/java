f3620cf6a1227558f2fd771e2dae29ce987fbd8d
ROHIT RAJ <147260754+roggtaugh@users.noreply.github.com>
2024-05-21 21:33:23 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Swap{
+  public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the number 1 : ");
+        int a=sc.nextInt();
+        System.out.println("enter the number 2 : ");
+        int b=sc.nextInt();
+        int c =a;
+        a=b;
+        b=c;
+        System.out.println("after swapping number 1 is :"+a+"and number 2 is :"+b);
+}
+}
