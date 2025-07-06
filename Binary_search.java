b9b5a08a7d3eb045fe6b5817e3e4290c8037bb62
rohit raj <roggtaugh@gmail.com>
2025-07-06 07:30:57 +0530

    initial commit

--- Binary_search.java
+++ Binary_search.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class Binary_search {
     public static void main(String[] args) {
+        System.out.println("the number you want to find is at " + binarySearch());
+    }
+    static int binarySearch(){
         Scanner input = new Scanner(System.in);
         System.out.print("enter the limit of array : ");
         int limit = input.nextInt();
@@ -15,9 +18,6 @@ public class Binary_search {
         System.out.println();
         System.out.print("enter the number you want to find : ");
         int num = input.nextInt();
-        System.out.println("the number you want to find is at " + binarySearch(arr, num, limit));
-    }
-    static int binarySearch(int[] arr, int num, int limit){
         for(int i =0; i < limit; i++){
             if(arr[i] == num){
                 return i;
